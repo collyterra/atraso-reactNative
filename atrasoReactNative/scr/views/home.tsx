143bdb0865a93f60369f541fc925ba6394029bd8
Guilherme-S-Mesquita <guimesquita.1512@gmail.com>
2024-09-18 16:00:01 -0300

    Agora temos o Scanner e falta ter um banco de dado

--- atrasoReactNative/scr/views/home.tsx
+++ atrasoReactNative/scr/views/home.tsx
@@ -1,15 +1,19 @@
-
 import React from 'react';
 import { View, Text, Button } from 'react-native';
+import Scanner from "../../components/Scanner/Scanner"; // ajuste o caminho se necessário
 
-const HomeScreen: React.FC<{ navigation: any }> = ({navigation}) =>{
+const HomeScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <Text>Home Screen</Text>
+      
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate('DesenvolvedoresScreen')}
       />
+
+      {/* Adicione o componente Scanner */}
+      <Scanner />
     </View>
   );
 };
